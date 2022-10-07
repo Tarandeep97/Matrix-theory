d6964262b767055e5afd9b8ccd8b2c63a841f20f
Tarandeep Singh <28994081+Tarandeep97@users.noreply.github.com>
2022-10-07 17:26:30 +0000

    Minor fixes for Echelon Form

--- julia2.jl
+++ julia2.jl
@@ -7,7 +7,7 @@ Addition_Mat = [0 1 2 3; 1 0 3 2; 2 3 0 1; 3 2 1 0]
 Identity_Mat = [0 0 0 0; 1 1 3 2; 2 2 1 3; 3 3 2 1]
 Mul_Mat = [0 0 0 0; 0 1 2 3; 0 2 3 1; 0 3 1 2]
 
-s_idx = 1
+s_idx = 7
 
 m = Samples[s_idx].m
 n = Samples[s_idx].n
@@ -19,24 +19,34 @@ Aug_Ab = hcat(A, b)
 
 fwd_steps = min(m,n)
 
-for i in 1:fwd_steps
+l=1
+i=1
+while (i<=fwd_steps && l<=n)
     #Partial Pivoting
-    max_val, max_idx = findmax(Aug_Ab[i:m, i])
+    max_val, max_idx = findmax(Aug_Ab[i:m, l])
+
+    if (max_val==0)
+        global l+=1
+        continue
+    end 
+
     if (max_idx!=1)
         (Aug_Ab[i, :], Aug_Ab[max_idx+i-1, :]) = (Aug_Ab[max_idx+i-1, :], Aug_Ab[i, :])
     end
 
     for j in i+1:m
-        if (Aug_Ab[j,i]!=0)
-            mul_tmp = Identity_Mat[Aug_Ab[j,i]+1, Aug_Ab[i,i]+1]
+        if (Aug_Ab[j,l]!=0)
+            mul_tmp = Identity_Mat[Aug_Ab[j,l]+1, Aug_Ab[i,l]+1]
 
-            Aug_Ab[j,i]=0
-            for k in i+1:n+1
+            Aug_Ab[j,l]=0
+            for k in l+1:n+1
                 tmp = Addition_Mat[Aug_Ab[j, k]+1, Mul_Mat[mul_tmp+1, Aug_Ab[i, k]+1]+1]
                 Aug_Ab[j, k] = tmp
             end
         end
     end
+    global i+=1
+    global l+=1
 end
 
 
@@ -46,10 +56,14 @@ U = Aug_Ab[:,1:n]
 U
 
 #Find rank
+l=1
+i=1
 rank_A=0
-for i in 1:fwd_steps
-    if (Aug_Ab[i,i]!=0)
-        global rank+=1
+while (i<=fwd_steps)
+    max_val, max_idx = findmax(Aug_Ab[i:m, l])
+    if (max_val==0)
+        global l+=1
+        continue
     end
 end
 
