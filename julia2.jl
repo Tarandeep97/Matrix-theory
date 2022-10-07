b083edc6e660631ad6f5cfee05fefaab6a472a94
Tarandeep Singh <28994081+Tarandeep97@users.noreply.github.com>
2022-10-07 11:12:27 +0000

    Fixes to echelon form

--- julia2.jl
+++ julia2.jl
@@ -14,8 +14,7 @@ b = rand(0:3, (m, 1))
 
 Aug_Ab = hcat(A, b)
 
-fwd_steps = min(m,n)-1
-all_zero_rows = Vector{Int64}()
+fwd_steps = min(m,n)
 
 for i in 1:fwd_steps
     #Partial Pivoting
@@ -24,27 +23,38 @@ for i in 1:fwd_steps
         (Aug_Ab[i, :], Aug_Ab[max_idx+i-1, :]) = (Aug_Ab[max_idx+i-1, :], Aug_Ab[i, :])
     end
 
-    
     for j in i+1:m
         if (Aug_Ab[j,i]!=0)
             mul_tmp = Identity_Mat[Aug_Ab[j,i]+1, Aug_Ab[i,i]+1]
 
             Aug_Ab[j,i]=0
-            zero_row_f = true
             for k in i+1:n+1
                 tmp = Addition_Mat[Aug_Ab[j, k]+1, Mul_Mat[mul_tmp+1, Aug_Ab[i, k]+1]+1]
-                if (tmp!=0)
-                    zero_row_f=false
-                end
                 Aug_Ab[j, k] = tmp
-            end
-
-            if (zero_row_f==true)
-                append!(all_zero_rows,j-i+1) #Fix needed here
             end
         end
     end
 end
 
-Aug_Ab
-all_zero_rows
+
+Aug_Ab 
+#Echelon form
+U = Aug_Ab[:,1:n] 
+U
+
+#Find rank
+rank_A=0
+for i in 1:fwd_steps
+    if (Aug_Ab[i,i]!=0)
+        global rank+=1
+    end
+end
+
+
+
+#Check consistency
+if (rank==m)
+    print("Consistent")
+elseif(rank<m)
+
+
