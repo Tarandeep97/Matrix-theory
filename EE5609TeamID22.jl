4df3026286a4ef0dcac40be0ea9c175d8573e507
Tarandeep Singh <28994081+Tarandeep97@users.noreply.github.com>
2022-10-09 06:34:08 +0000

    Function created

---
+++ EE5609TeamID22.jl
@@ -0,0 +1,87 @@
+
+
+function rankconsistencyTeamID22(A::Vector{Int64},b::Vector{Int64})
+    m,n = size(A)
+
+    #Helper Matrix for Addition,Find Multiple to make zero, Multiplication
+    Addition_Mat = [0 1 2 3; 1 0 3 2; 2 3 0 1; 3 2 1 0]
+    Identity_Mat = [0 0 0 0; 1 1 3 2; 2 2 1 3; 3 3 2 1]
+    Mul_Mat = [0 0 0 0; 0 1 2 3; 0 2 3 1; 0 3 1 2]
+
+    #Augmented Matrix
+    Aug_Ab = hcat(A, b)
+    fwd_steps = min(m,n)
+
+    l=1
+    i=1
+    while (i<=fwd_steps && l<=n)
+        #Partial Pivoting
+        max_val, max_idx = findmax(Aug_Ab[i:m, l])
+
+        if (max_val==0)
+            l+=1
+            continue
+        end 
+
+        if (max_idx!=1)
+            (Aug_Ab[i, :], Aug_Ab[max_idx+i-1, :]) = (Aug_Ab[max_idx+i-1, :], Aug_Ab[i, :])
+        end
+
+        for j in i+1:m
+            if (Aug_Ab[j,l]!=0)
+                mul_tmp = Identity_Mat[Aug_Ab[j,l]+1, Aug_Ab[i,l]+1]
+
+                Aug_Ab[j,l]=0
+                for k in l+1:n+1
+                    tmp = Addition_Mat[Aug_Ab[j, k]+1, Mul_Mat[mul_tmp+1, Aug_Ab[i, k]+1]+1]
+                    Aug_Ab[j, k] = tmp
+                end
+            end
+        end
+        i+=1
+        l+=1
+    end
+
+    U = Aug_Ab[:,1:n]
+    
+    #Find rank
+    l=1
+    i=1
+    rank_A=0
+    rank_Ab=0
+    while (i<=fwd_steps && l<=n)
+        if (Aug_Ab[i,l]!=0)
+            rank_A+=1
+            rank_Ab+=1
+            i+=1
+            l+=1
+        else
+            if (Aug_Ab[i,n+1]!=0)
+                rank_Ab+=1
+            end 
+            l+=1
+        end
+    end
+
+    #Check consistency
+    consistent = false
+    if (rank_A==rank_Ab)
+        consistent=true
+    end
+
+    return U,rank_A,consistent
+end
+
+
+using JLD2
+@load "Samples_Prog_1.jld2"
+
+s_idx = 14
+
+m = Samples[s_idx].m
+n = Samples[s_idx].n
+
+A = Samples[s_idx].A
+b = Samples[s_idx].b
+
+print(rankconsistencyTeamID22(A,b))
--- julia2.jl
+++ julia2.jl
@@ -7,7 +7,7 @@ Addition_Mat = [0 1 2 3; 1 0 3 2; 2 3 0 1; 3 2 1 0]
 Identity_Mat = [0 0 0 0; 1 1 3 2; 2 2 1 3; 3 3 2 1]
 Mul_Mat = [0 0 0 0; 0 1 2 3; 0 2 3 1; 0 3 1 2]
 
-s_idx = 25
+s_idx = 14
 
 m = Samples[s_idx].m
 n = Samples[s_idx].n
@@ -59,22 +59,22 @@ U = Aug_Ab[:,1:n]
 l=1
 i=1
 rank_A=0
+rank_Ab=0
 while (i<=fwd_steps && l<=n)
     if (Aug_Ab[i,l]!=0)
         global rank_A+=1
+        global rank_Ab+=1
         global i+=1
         global l+=1
     else
+        if (Aug_Ab[i,n+1]!=0)
+            global rank_Ab+=1
+        end 
         global l+=1
     end
 end
 print(rank_A,Samples[s_idx].r)
 
-if (Aug_Ab[i,l]!=0)
-    rank_Ab = rank_A + 1
-else
-    rank_Ab = rank_A
-end
 
 #Check consistency
 if (rank_A==rank_Ab)
@@ -83,4 +83,4 @@ else
     print("Inconsistent")
 end
 
-
+print(Samples[s_idx])
