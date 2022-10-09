d54e01d2a550b97bfc3f3f5c1a14add18626edee
Tarandeep Singh <28994081+Tarandeep97@users.noreply.github.com>
2022-10-09 05:38:17 +0000

    Fixes to echelon form

--- julia2.jl
+++ julia2.jl
@@ -7,7 +7,7 @@ Addition_Mat = [0 1 2 3; 1 0 3 2; 2 3 0 1; 3 2 1 0]
 Identity_Mat = [0 0 0 0; 1 1 3 2; 2 2 1 3; 3 3 2 1]
 Mul_Mat = [0 0 0 0; 0 1 2 3; 0 2 3 1; 0 3 1 2]
 
-s_idx = 1
+s_idx = 25
 
 m = Samples[s_idx].m
 n = Samples[s_idx].n
@@ -68,7 +68,7 @@ while (i<=fwd_steps && l<=n)
         global l+=1
     end
 end
-
+print(rank_A,Samples[s_idx].r)
 
 if (Aug_Ab[i,l]!=0)
     rank_Ab = rank_A + 1
